bb69130691d0c8f096bc9afd85393af54f1489ef
Victor Torres <torres.victor136@gmail.com>
2020-07-11 20:25:04 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,18 +1,17 @@
 class Person
-    def name=(persons_name)
-      @name = persons_name
-    end
 
-    def name
-      @name
-    end
+  def name=(name)
+    @name = name
+  end
+  def name
+    @name
+  end
 
-    def job=(persons_job)
-      @job = persons_job
-    end
+  def job=(job)
+    @job = job
+  end
 
-    def job
-      @job
-    end
-
+  def job
+    @job
+  end
 end
