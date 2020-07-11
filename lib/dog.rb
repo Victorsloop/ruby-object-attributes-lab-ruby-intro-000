caaddc2f470f247e823da5ded4cb65a385e6f2c8
Victor Torres <torres.victor136@gmail.com>
2020-07-11 20:24:46 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,20 @@
 class Dog
-  def name = (dog_name)
-   @name = dog_name
- end
 
- def name
-   @name
- end
+  def name=(name)
+    @name = name
+  end
 
- def breed=(dog_breed)
-   @breed = dog_breed
- end
+  def name
+    @name
+  end
 
- def breed
-   @breed
- end
+  def breed=(breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
 
 
 end
